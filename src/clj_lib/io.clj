60c184345cbecc44e5add5d5807c3151480b3e6b
Ryan Wilson <ryanwilson.m@gmail.com>
2016-01-04 21:41:23 -0800

    bug fix, forgot to unquote a symbol in new macros

--- src/clj_lib/io.clj
+++ src/clj_lib/io.clj
@@ -134,12 +134,12 @@
   "Executes `body` in a context in which `*in*` is bound to a reader of `s`. See
   See `clojure.java.io/reader` for supported argument types."
   [s & body]
-  `(binding [*in* (java.io/reader s)]
+  `(binding [*in* (java.io/reader ~s)]
      ~@body))
 
 (defmacro with-out
   "Executes `body` in a context in which `*out*` is bound to a writer to `s`.
   See `clojure.java.io/writer` for supported argument types."
   [s & body]
-  `(binding [*out* (java.io/writer s)]
+  `(binding [*out* (java.io/writer ~s)]
      ~@body))
